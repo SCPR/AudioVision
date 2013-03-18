deaabeb1e5848e163562521fad3404114c0ca9f0
Bryan Ricker <bricker88@gmail.com>
2013-03-18 09:06:41 -0700

    Add schema; Fix migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20130318091018) do
+
+  create_table "authors", force: true do |t|
+    t.text    "bio"
+    t.integer "user_id"
+    t.string  "name"
+    t.string  "slug"
+  end
+
+  add_index "authors", ["slug"], name: "index_authors_on_slug"
+  add_index "authors", ["user_id"], name: "index_authors_on_user_id"
+
+  create_table "flatpages", force: true do |t|
+    t.string   "path"
+    t.string   "title"
+    t.string   "description"
+    t.string   "content"
+    t.string   "redirect_to"
+    t.string   "extra_head"
+    t.string   "extra_footer"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "flatpages", ["path"], name: "index_flatpages_on_path"
+
+  create_table "post_assets", force: true do |t|
+    t.integer  "asset_id"
+    t.integer  "post_id"
+    t.integer  "position"
+    t.string   "caption"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "post_assets", ["post_id", "position"], name: "index_post_assets_on_post_id_and_position"
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.string   "slug"
+    t.integer  "status"
+    t.text     "body"
+    t.datetime "published_at"
+    t.text     "teaser"
+    t.string   "media_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "posts", ["media_type"], name: "index_posts_on_media_type"
+  add_index "posts", ["status", "published_at"], name: "index_posts_on_status_and_published_at"
 
 end
