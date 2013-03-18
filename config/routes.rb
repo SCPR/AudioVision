81f56bc328222784146a3ee5c115ceb59bae093d
Bryan Ricker <bricker88@gmail.com>
2013-03-18 09:06:47 -0700

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
-AudioVision::Application.routes.draw do
+  AudioVision::Application.routes.draw do
   root to: 'home#index'
 
   get '/slideshows'  => 'posts#index', defaults: { media_type: "slideshow" }
-  get '/:id/:slug'   => 'posts#show'
+  get '/:id(/:slug)'   => 'posts#show', constraints: { id: /\d+/ }
 
   get '/staff'       => 'authors#index'
   get '/staff/:slug' => 'authors#show'
