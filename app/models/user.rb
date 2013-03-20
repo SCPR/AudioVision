29e944b949c3a7d8a8125bde6d126e9dc8ac56dd
Bryan Ricker <bricker88@gmail.com>
2013-03-19 17:46:15 -0700

    Share auth with mercer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  establish_connection "mercer_#{Rails.env}"
+  self.table_name = "auth_user"
+  
   include Outpost::Model::Authorization
   include Outpost::Model::Authentication
 
