d29bb57e8de1d51028d5e8a09d7144eef7ec31b5
Jon White <mr.jon.white@gmail.com>
2013-03-29 11:00:46 -0700

    Completed the Sticky Ledge functionality

--- public/_front-end-comps/static-assets/js/scripts.js
+++ public/_front-end-comps/static-assets/js/scripts.js
@@ -2,35 +2,55 @@ jQuery(document).ready(function($) {
 
 
 
-	/*
-	/* Masthead & Ledge UI
-	$(".masthead .inside, .ledge .nav-av .menu, .ledge .nav-kpcc .inside").each(function(){
-			$(this).hover(function(){
-				$(this).toggleClass("revealed");
-			}).click(function(){
-				$(this).toggleClass("revealed");
-			}).blur(function(){
-				$(this).removeClass("revealed");
-			});
-	});
-	*/
+
 	
+/*	-----------------------------------------------------------------------------------------------------------------
+	Are we tapping or clicking?
+	----------------------------------------------------------------------------------------------------------------- */
 	function is_touch_device() {
 	  return !!('ontouchstart' in window) // works on most browsers 
 	      || !!('onmsgesturechange' in window); // works on ie10
 	};
-	
+
+
+
+
+/*	-----------------------------------------------------------------------------------------------------------------
+	Masthead & Ledge interactions
+	----------------------------------------------------------------------------------------------------------------- */
 	if(is_touch_device() == true) {
-		alert("yes, you're on a touch device");
+
+			$(".masthead .inside, .ledge .nav-av .menu, .ledge .nav-kpcc .inside").each(function(){
+				$(this).click(function(){
+					if($(this).hasClass("revealed")) {
+						$(this).removeClass("revealed");
+					} else {
+						$(".masthead .inside, .ledge .nav-av .menu, .ledge .nav-kpcc .inside").removeClass("revealed");
+						$(this).addClass("revealed");
+					}
+				});
+			});
+
 	} else {
-		alert("this is a mouse device, not for tapping");
+			
+			$(".masthead .inside, .ledge .nav-av .menu, .ledge .nav-kpcc .inside").each(function(){
+				$(this).hover(function(){
+					$(this).toggleClass("revealed");
+				});
+			});
+
 	}
 
 
-	
 
 
 
+/*	-----------------------------------------------------------------------------------------------------------------
+	Ledge stickiness
+	----------------------------------------------------------------------------------------------------------------- */
+	$(".ledge").waypoint("sticky", {
+	  offset: 30 // Apply "stuck" when element 30px from top
+	});
 
 
 });
