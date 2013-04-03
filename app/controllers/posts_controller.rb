bd0a3c34b8c7aa0335100aa34692707d9ce9f889
Bryan Ricker <bricker88@gmail.com>
2013-04-03 11:50:01 -0700

    Add partials for media types

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,11 @@
 class PostsController < ApplicationController
+  FakePost = Struct.new(:media_type, :related_articles)
+
   def index
   end
 
   def show
+    @post = FakePost.new(params[:media_type] || "image", params[:related_articles])
   end
 
   def archive
