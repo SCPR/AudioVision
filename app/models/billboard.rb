22a03a81b5be79fb9809dcc1bcaa50d6d7c01db5
Bryan Ricker <bricker88@gmail.com>
2013-04-04 12:04:01 -0700

    Update billboard descriptions to include the number

--- app/models/billboard.rb
+++ app/models/billboard.rb
@@ -1,12 +1,12 @@
 class Billboard < ActiveRecord::Base
   LAYOUTS = {
-    1 => "TOP: Slideshow / BOTTOM: Video (16:9), Mobile (1:1)",
-    2 => "TOP: Slideshow / BOTTOM: Triptych of Images (3:2)",
-    3 => "TOP: Slideshow / BOTTOM: Pair of Videos (16:9)",
-    4 => "TOP: Video (16:9) + Mobile (1:1) / BOTTOM: Triptych of Images (3:2)",
-    5 => "TOP: Mobile (1:1) + Video (16:9) / BOTTOM: Triptych of Images (3:2)",
-    6 => "TOP: Mobile (1:1) + Image (3:2) / BOTTOM: Pair of Videos (16:9)",
-    7 => "TOP: Image (3:2) + Mobile (1:1) / BOTTOM: Pair of Videos (16:9)"
+    1 => "(1) TOP: Slideshow / BOTTOM: Video (16:9), Mobile (1:1)",
+    2 => "(2) TOP: Slideshow / BOTTOM: Triptych of Images (3:2)",
+    3 => "(3) TOP: Slideshow / BOTTOM: Pair of Videos (16:9)",
+    4 => "(4) TOP: Video (16:9) + Mobile (1:1) / BOTTOM: Triptych of Images (3:2)",
+    5 => "(5) TOP: Mobile (1:1) + Video (16:9) / BOTTOM: Triptych of Images (3:2)",
+    6 => "(6) TOP: Mobile (1:1) + Image (3:2) / BOTTOM: Pair of Videos (16:9)",
+    7 => "(7) TOP: Image (3:2) + Mobile (1:1) / BOTTOM: Pair of Videos (16:9)"
   }
 
   STATUS = {
@@ -23,7 +23,7 @@ class Billboard < ActiveRecord::Base
 
   scope :published, -> { where(status: STATUS[:published]) }
 
-
+  validates :layout, presence: true, inclusion: { in: LAYOUTS.keys }
   has_many :billboard_posts
   has_many :posts, through: :billboard_posts
 end
