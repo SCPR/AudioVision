3697663359e6c997f81d5fe664428f1ededb7b19
Bryan Ricker <bricker88@gmail.com>
2013-04-04 09:36:07 -0700

    Add custom errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include Outpost::Controller::CustomErrors
+
+  def render_error(status, e=StandardError)
+    if Rails.application.config.consider_all_requests_local
+      raise e
+    else
+      respond_to do |format|
+        format.html { render template: "/errors/error_#{status}", status: status, locals: { error: e } }
+        format.xml { render xml: { error: status.to_s }, status: status }
+        format.text { render text: status, status: status}
+      end
+      
+      report_error(e)
+    end
+  end
 end
