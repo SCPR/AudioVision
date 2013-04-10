0aec22033b4cf541a591d01b5dd7fbd855c7f4b8
Bryan Ricker <bricker88@gmail.com>
2013-04-10 15:23:13 -0700

    Setup Content API and aggregator

--- app/controllers/api/public/v1/posts_controller.rb
+++ app/controllers/api/public/v1/posts_controller.rb
@@ -3,13 +3,29 @@ module Api::Public::V1
     respond_to :json
 
     def index
-      @posts = Post.published
-      respond_with @posts
+      @posts = Post.published.page(params[:page]).per(params[:limit])
+      respond_with @posts.map { |post| post_json(post) }
     end
 
     def show
       @post = Post.find(params[:id])
-      respond_with @post
+      respond_with post_json(@post)
+    end
+
+    private
+
+    def post_json(post)
+      {
+        :id           => post.obj_key,
+        :title        => post.to_title,
+        :published_at => post.published_at,
+        :teaser       => post.teaser,
+        :body         => post.body,
+        :permalink    => post.remote_link_path,
+        :asset        => post.asset.lsquare.tag,
+        :byline       => post.byline,
+        :edit_link    => post.admin_edit_path
+      }
     end
   end
 end
--- app/models/billboard.rb
+++ app/models/billboard.rb
@@ -26,8 +26,10 @@ class Billboard < ActiveRecord::Base
   validates :layout, presence: true, inclusion: { in: LAYOUTS.keys }
   validates :status, presence: true
   
-  has_many :post_references, as: :referrer
+  has_many :post_references, as: :referrer, order: "position", dependent: :destroy
   has_many :posts, through: :post_references
+
+  accepts_json_input_for_content(name: :post_references)
 
   class << self
     def current
@@ -41,5 +43,16 @@ class Billboard < ActiveRecord::Base
     def status_collection
       STATUS_TEXT.map { |k, v| [v, k] }
     end
+  end
+
+
+  private
+
+  def build_content_association(content_hash, content)
+    PostReference.new(
+      :post       => content,
+      :referrer   => self,
+      :position   => content_hash['position'].to_i
+    )
   end
 end
