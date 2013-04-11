b939d5f89267517ba273f31536aef5cd129726c0
Bryan Ricker <bricker88@gmail.com>
2013-04-11 13:10:41 -0700

    Remove alternate formats for /category - use /feed/category for that.

--- app/controllers/category_handler.rb
+++ app/controllers/category_handler.rb
@@ -1,21 +1,7 @@
 module CategoryHandler
   def handle_category
     @nav_highlight = @category.slug
-
-    @posts = @category.posts.published.page(params[:page]).per(13)
-
-    if request.format.xml?
-      @feed = {
-        :title          => @category.title,
-        :href           => @category.remote_link_path,
-        :description    => @category.description
-      }
-    end
-
-    respond_with @posts do |format|
-      format.html { render "posts/category" }
-      format.xml { render "posts/archive" }
-      format.json
-    end
+    @posts = @category.posts.published.limit(13)
+    render 'posts/category'
   end
 end
