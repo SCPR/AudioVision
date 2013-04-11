49dfd75e8dc53235a001d66fb33a8b1aef7c3a50
Bryan Ricker <bricker88@gmail.com>
2013-04-10 17:56:28 -0700

    Setup Buckets

--- app/models/bucket.rb
+++ app/models/bucket.rb
@@ -3,4 +3,14 @@ class Bucket < ActiveRecord::Base
   
   has_many :post_references, as: :referrer
   has_many :posts, through: :post_references
+
+  accepts_json_input_for_content(name: :post_references)
+
+  def build_content_association(content_hash, content)
+    PostReference.new(
+      :post       => content,
+      :referrer   => self,
+      :position   => content_hash['position'].to_i
+    )
+  end
 end
