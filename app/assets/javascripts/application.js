3bf2770df76e9e833efe679defd8445db298fc90
Bryan Ricker <bricker88@gmail.com>
2013-04-11 18:18:53 -0700

    Work on cleaning up Slideshow script/templates.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,16 @@
-//= require base
 //= require jquery
+//= require underscore
+//= require backbone
 //= require jquery_ujs
+
+//= require base
+//= require scripts
+
+//= require_tree ./slideshow/templates
+//= require slideshow/slideshow
 
 //= require waypoints.min
 //= require waypoints-sticky.min
 //= require fancybox/fancybox
 //= require fancybox/helpers/fancybox-buttons
 //= require fancybox/helpers/fancybox-thumbs
-
-//= require scripts
