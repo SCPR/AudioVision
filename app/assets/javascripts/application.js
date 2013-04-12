0698f1281a3057a3229bd471604f26fdd35e7640
Bryan Ricker <bricker88@gmail.com>
2013-04-11 17:09:16 -0700

    Import slideshow script from AssetHopst

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,11 @@
 //= require base
 //= require jquery
 //= require jquery_ujs
+
 //= require waypoints.min
 //= require waypoints-sticky.min
 //= require fancybox/fancybox
 //= require fancybox/helpers/fancybox-buttons
 //= require fancybox/helpers/fancybox-thumbs
+
 //= require scripts
