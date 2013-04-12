7ec0600d9bb31ba7ee759db20cc733d732302465
Bryan Ricker <bricker88@gmail.com>
2013-04-12 16:10:06 -0700

    Add byline rendering

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def show
-    @post       = Post.find(params[:id])
+    @post       = Post.includes(:attributions).find(params[:id])
     @category   = @post.category
 
     @nav_highlight = @category.slug
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,25 @@ module ApplicationHelper
 
     content_tag :li, link_to(*args), class: ("selected" if @nav_highlight == key)
   end
+
+
+  def render_byline(post)
+    attributions = post.attributions.where('role in (?)', [Attribution::ROLE_AUTHOR, Attribution::ROLE_SOURCE]).order('role')
+    byline_elements = []
+
+    attributions.each do |attribution|
+      if (reporter = attribution.reporter)
+        byline_elements << link_to(reporter.name, reporter.link_path)
+
+      elsif attribution.name.present?
+        if attribution.url.present?
+          byline_elements << link_to(attribution.name, attribution.url)
+        else
+          byline_elements << attribution.name
+        end
+      end
+    end
+
+    byline_elements.join(" / ").html_safe
+  end
 end
