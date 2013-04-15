941462c0bc315ed66f1199eb2e77c7f1db39713b
Bryan Ricker <bricker88@gmail.com>
2013-04-15 14:39:29 -0700

    Preview for Billboards

--- app/controllers/outpost/billboards_controller.rb
+++ app/controllers/outpost/billboards_controller.rb
@@ -1,6 +1,21 @@
 class Outpost::BillboardsController < Outpost::ResourceController
   outpost_controller
 
+  def preview
+    @billboard = Outpost.obj_by_key(params[:obj_key]) || Billboard.new
+    
+    with_rollback @billboard do
+      @billboard.assign_attributes(form_params)
+
+      if @billboard.valid?
+        render "/billboards/_billboard", layout: "outpost/preview/application", locals: { billboard: @billboard }
+      else
+        render_preview_validation_errors(@billboard)
+      end
+    end
+  end
+
+
   private
 
   def form_params
