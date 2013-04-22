40483f94d09c82fc407b6236a55dba2685a77b27
Bryan Ricker <bricker88@gmail.com>
2013-04-22 09:13:58 -0700

    Setup error rendering; add empty auth method for private API

--- app/controllers/api/public/v1/base_controller.rb
+++ app/controllers/api/public/v1/base_controller.rb
@@ -3,6 +3,29 @@ module Api::Public::V1
   TYPE      = "public"
 
   class BaseController < ::ActionController::Base
+    respond_to :json
 
+    private
+
+    #---------------------------
+
+    def render_not_found(options={})
+      message = options[:message] || "Not Found"
+      render status: :not_found, json: { error: message }
+    end
+
+    #---------------------------
+
+    def render_bad_request(options={})
+      message = options[:message] || "Bad Request"
+      render status: :bad_request, json: { error: message }
+    end
+
+    #---------------------------
+
+    def render_unauthorized(options={})
+      message = options[:message] || "Unauthorized"
+      render status: :unauthorized, json: { error: message }
+    end
   end
 end
