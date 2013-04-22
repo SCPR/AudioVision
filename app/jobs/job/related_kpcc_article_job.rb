b75c08802cf89c6a390c51cc8b8a2a5b4d667e5e
Bryan Ricker <bricker88@gmail.com>
2013-04-22 15:26:47 -0700

    Better logging

--- app/jobs/job/related_kpcc_article_job.rb
+++ app/jobs/job/related_kpcc_article_job.rb
@@ -5,7 +5,7 @@ module Job
     # Pass in the obj_key of the post that this is for, 
     # and the URL we want to fetch.
     def self.perform(id)
-      puts "Performing RelatedKpccArticleJob"
+      puts "Performing RelatedKpccArticleJob for Post #{id}"
       post = Post.find(id)
       post.cache_related_kpcc_article_json
     end
