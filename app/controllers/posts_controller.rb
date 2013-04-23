54c644ec17278feb43b99dcc4b42493855b748b6
Bryan Ricker <bricker88@gmail.com>
2013-04-23 15:55:00 -0700

    Better var name

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,15 @@
 class PostsController < ApplicationController
+  respond_to :html, :json
+
   def show
     @post       = Post.includes(:attributions, :category).find(params[:id])
     @category   = @post.category
 
     @nav_highlight = @category.slug
     
-    @recent_posts_this_type      = Post.published.where("category_id = ?", @category.id).where('id != ?', @post.id).limit(2)
-    @recent_posts_not_this_type  = Post.published.where("category_id != ?", @category.id).limit(4)
+    @recent_posts_this_category      = Post.published.where("category_id = ?", @category.id).where('id != ?', @post.id).limit(2)
+    @recent_posts_not_this_category  = Post.published.where("category_id != ?", @category.id).limit(4)
 
-    respond_to do |format|
-      format.html
-      format.json
-    end
+    respond_with @post
   end
 end
