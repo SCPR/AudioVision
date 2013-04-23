aaa80a892ab99bd953566cf31b2cd896fd7a6e66
Bryan Ricker <bricker88@gmail.com>
2013-04-23 15:54:29 -0700

    Specs

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,4 +1,71 @@
 require 'spec_helper'
 
 describe PostsController do
+  describe 'GET show' do
+    render_views
+
+    let(:category1) { create :category }
+    let(:category2) { create :category }
+    let(:post) { create :post, category: category1 }
+
+    it 'finds the post' do
+      get :show, post.route_hash
+      assigns(:post).should eq post
+    end
+
+    it "sets the recent posts, and does not include this post" do
+      posts_category1 = create_list :post, 1, category: category1
+      posts_category2 = create_list :post, 1, category: category2
+
+      get :show, post.route_hash
+      assigns(:recent_posts_this_category).should eq posts_category1
+      assigns(:recent_posts_not_this_category).should eq posts_category2
+    end
+
+    it "can respond with json" do
+      get :show, post.route_hash.merge(format: :json)
+      response.should render_template 'show'
+      response.headers['Content-Type'].should match /json/
+    end
+
+    context "for image" do
+      it "renders properly" do
+        post = create :post, post_type: Post::POST_TYPES[:image]
+        create_list :post_asset, 3, post: post
+        get :show, post.route_hash
+
+        response.should render_template partial: 'posts/post_types/_image'
+      end
+    end
+
+    context "for slideshow" do
+      it "renders properly" do
+        post = create :post, post_type: Post::POST_TYPES[:slideshow]
+        create_list :post_asset, 3, post: post
+        get :show, post.route_hash
+
+        response.should render_template partial: 'posts/post_types/_slideshow'
+      end
+    end
+
+    context "for video" do
+      it "renders properly" do
+        post = create :post, post_type: Post::POST_TYPES[:video]
+        create_list :post_asset, 3, post: post
+        get :show, post.route_hash
+
+        response.should render_template partial: 'posts/post_types/_video'
+      end
+    end
+
+    context "for gallery" do
+      it "renders properly" do
+        post = create :post, post_type: Post::POST_TYPES[:gallery]
+        create_list :post_asset, 3, post: post
+        get :show, post.route_hash
+
+        response.should render_template partial: 'posts/post_types/_gallery'
+      end
+    end
+  end
 end
--- spec/controllers/reporters_controller_spec.rb
+++ spec/controllers/reporters_controller_spec.rb
@@ -1,4 +1,33 @@
 require 'spec_helper'
 
 describe ReportersController do
+  describe 'GET index' do
+    render_views
+
+    it 'grabs only is_listed reporters' do
+      listed    = create :reporter, is_listed: true
+      unlisted  = create :reporter, is_listed: false
+
+      get :index
+      assigns(:reporters).should eq [listed]
+    end
+  end
+
+  describe 'GET show' do
+    render_views
+
+    it 'finds reporter by slug and assigns his posts' do
+      reporter           = create :reporter
+      published_post     = create :post, status: Post::STATUS[:published]
+      unpublished_post   = create :post, status: Post::STATUS[:draft]
+
+      create :attribution, reporter: reporter, post: published_post
+      create :attribution, reporter: reporter, post: unpublished_post
+
+      get :show, reporter.route_hash
+
+      assigns(:reporter).should eq reporter
+      assigns(:posts).should eq [published_post]
+    end
+  end
 end
