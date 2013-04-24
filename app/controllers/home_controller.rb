194070faa06d6a7bf5a72232a033dde0daaf7217
Bryan Ricker <bricker88@gmail.com>
2013-04-23 17:39:23 -0700

    Optimizations to home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,14 +2,13 @@ class HomeController < ApplicationController
   def homepage
     @nav_highlight = "home"
 
-    @recent_posts   = Post.published.limit(15)
-    @billboard      = Billboard.published.first
-    
-    @midway_bucket    = Bucket.where(key: "instagram").first
-    @right_bar_bucket = Bucket.where(key: "featured-posts").first
+    @billboard          = Billboard.published.includes(:post_references).first
+    @recent_posts       = Post.published.limit(15)
+    @midway_bucket      = Bucket.where(key: "instagram").first
+    @right_bar_bucket   = Bucket.where(key: "featured-posts").first
 
     # If we have a billboard, then don't show any of its posts in "Recent"
-    if @billboard.present?
+    if @billboard.present? && @billboard.post_references.present?
       @recent_posts.where!("id not in (?)", @billboard.post_references.map(&:post_id))
     end
   end
