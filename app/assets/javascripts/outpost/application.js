deb6af2de74ddbc3f75f9e28c66e1b8245a93aec
Bryan Ricker <bricker88@gmail.com>
2013-04-26 00:07:36 -0700

    Add script for auto-loading the KPCC article on URL field blur

--- app/assets/javascripts/outpost/application.js
+++ app/assets/javascripts/outpost/application.js
@@ -2,7 +2,10 @@
 //= require outpost
 //= require outpost/content_api
 //= require outpost/asset_host/assethostbase
+
+//= require_tree ./templates/
 //= require outpost/asset_host
 //= require outpost/aggregator
 //= require outpost/publishing
 //= require outpost/content_alarm
+//= require outpost/related_kpcc_article_feedback
