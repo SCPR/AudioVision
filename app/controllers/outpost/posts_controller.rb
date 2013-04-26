a37924b019c0fad9ca16f1acfcbba00342826e4a
Bryan Ricker <bricker88@gmail.com>
2013-04-26 08:53:56 -0700

    Specify default order and sort mode on posts index; make columns sortable

--- app/controllers/outpost/posts_controller.rb
+++ app/controllers/outpost/posts_controller.rb
@@ -2,12 +2,15 @@ class Outpost::PostsController < Outpost::ResourceController
   outpost_controller
 
   define_list do |l|
+    l.default_order = "updated_at"
+    l.default_sort_mode = "desc"
+
     l.column :title
     l.column :post_type, header: "Type", display: ->(r) { Post::POST_TYPES_TEXT[r.post_type] }
     l.column :byline
     l.column :status
-    l.column :published_at
-    l.column :updated_at
+    l.column :published_at, sortable: true, default_sort_mode: "desc"
+    l.column :updated_at, sortable: true, default_sort_mode: "desc"
 
     l.filter :post_type, collection: -> { Post.post_types_collection }
     l.filter :status, collection: -> { Post.status_collection }
