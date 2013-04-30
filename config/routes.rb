9f63f6cf025798cb659b403bfac8abcfb8930eff
Bryan Ricker <bricker88@gmail.com>
2013-04-29 17:25:46 -0700

    Import some outpost routes

--- config/routes.rb
+++ config/routes.rb
@@ -37,12 +37,6 @@ AudioVision::Application.routes.draw do
 
   ## OUTPOST
   namespace :outpost do
-    root to: 'home#dashboard'
-    
-    resources :sessions, only: [:create, :destroy]
-    get 'login'  => "sessions#new", as: :login
-    get 'logout' => "sessions#destroy", as: :logout
-
     resources :posts, concerns: [:previewable]
     resources :billboards, concerns: [:previewable]
 
@@ -51,7 +45,13 @@ AudioVision::Application.routes.draw do
     resources :users
     resources :categories
     resources :buckets
+
+    root to: 'home#dashboard'
     
+    resources :sessions, only: [:create, :destroy]
+    get 'login'  => "sessions#new", as: :login
+    get 'logout' => "sessions#destroy", as: :logout
+
     get "*path" => 'errors#not_found'
   end
 
