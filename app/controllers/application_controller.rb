bb935952e5e2569f5c3ad796afd9d48233362467
Bryan Ricker <bricker88@gmail.com>
2013-04-29 17:06:11 -0700

    Super to Outpost for render_error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,7 @@ class ApplicationController < ActionController::Base
   include Outpost::Controller::CustomErrors
 
   def render_error(status, e=StandardError)
-    if Rails.application.config.consider_all_requests_local
-      raise e
-    else
-      respond_to do |format|
-        format.html { render template: "/errors/error_#{status}", status: status, locals: { error: e } }
-        format.xml { render xml: { error: status.to_s }, status: status }
-        format.text { render text: status, status: status}
-      end
-      
-      report_error(e)
-    end
+    super
+    report_error(e)
   end
 end
