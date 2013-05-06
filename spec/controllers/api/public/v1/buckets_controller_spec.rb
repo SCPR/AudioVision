f9f0626af4a104f1e214a6407791f9a5023b9361
Bryan Ricker <bricker88@gmail.com>
2013-05-06 09:31:53 -0700

    Specs for buckets API

--- spec/controllers/api/public/v1/buckets_controller_spec.rb
+++ spec/controllers/api/public/v1/buckets_controller_spec.rb
@@ -5,13 +5,33 @@ describe Api::Public::V1::BucketsController do
     :format => :json
   }
 
+  describe "GET show" do
+    it "finds the object if it exists" do
+      bucket = create :bucket
+      bucket.posts = create_list :post, 2
+
+      get :show, { id: bucket.key }.merge(request_params)
+      
+      assigns(:bucket).should eq bucket
+      response.should render_template "show"
+    end
+
+    it "returns a 404 status if it does not exist" do
+      get :show, { id: "nope" }.merge(request_params)
+      response.response_code.should eq 404
+      response.body.should eq Hash[error: "Not Found"].to_json
+    end
+  end
+
 
   describe "GET index" do
-    it "Gets all of the buckets" do
-      buckets = create_list :bucket, 2
-      
+    before :each do
+      @buckets = create_list :bucket, 2
+    end
+
+    it "get the buckets" do
       get :index, request_params
-      assigns(:buckets).should eq buckets
+      assigns(:buckets).to_a.should eq @buckets
     end
   end
 end
