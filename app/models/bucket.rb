53133bd75b5eea35f6c30ad97bbcec1cbdb6cb07
Bryan Ricker <bricker88@gmail.com>
2013-05-06 09:31:05 -0700

    Limit buckets to only return published posts

--- app/models/bucket.rb
+++ app/models/bucket.rb
@@ -2,7 +2,7 @@ class Bucket < ActiveRecord::Base
   outpost_model
   
   has_many :post_references, as: :referrer
-  has_many :posts, through: :post_references
+  has_many :posts, -> { where(status: Post::STATUS[:published]) }, through: :post_references
 
   accepts_json_input_for_content(name: :post_references)
 
