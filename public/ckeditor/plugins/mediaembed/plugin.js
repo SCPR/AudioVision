d7e51306ad2de152a0b1e9cbe134ce42c4d36312
Bryan Ricker <bricker88@gmail.com>
2013-08-15 23:30:16 -0700

    Update to CKEditor 4.2; remove a bunch of unused plugins

--- public/ckeditor/plugins/mediaembed/plugin.js
+++ public/ckeditor/plugins/mediaembed/plugin.js
@@ -8,11 +8,13 @@
 * http://paulrobertlloyd.com/
 *
 * @author Fabian Vogelsteller [frozeman.de]
-* @version 0.4
+* @version 0.5
 */
 ( function() {
     CKEDITOR.plugins.add( 'mediaembed',
     {
+        icons: 'mediaembed', // %REMOVE_LINE_CORE%
+        hidpi: true, // %REMOVE_LINE_CORE%
         init: function( editor )
         {
            var me = this;
@@ -30,7 +32,7 @@
                              elements :[{
                                 id : 'embedArea',
                                 type : 'textarea',
-                                label : 'Paste Embed Code Here (Allowed: iframe, div, object, embed)',
+                                label : 'Paste Embed Code Here',
                                 'autofocus':'autofocus',
                                 setup: function(element){
                                 },
@@ -46,20 +48,21 @@
                             }
                         }
                         // console.log(this.getContentElement( 'iframe', 'embedArea' ).getValue());
-                        div = instance.document.createElement('p');
+                        div = instance.document.createElement('div');
                         div.setHtml(this.getContentElement('iframe', 'embedArea').getValue());
                         instance.insertElement(div);
                   }
               };
            } );
 
-            editor.addCommand( 'MediaEmbed', new CKEDITOR.dialogCommand( 'MediaEmbedDialog' ) );
+            editor.addCommand( 'MediaEmbed', new CKEDITOR.dialogCommand( 'MediaEmbedDialog',
+                { allowedContent: 'iframe[*]' }
+            ) );
 
             editor.ui.addButton( 'MediaEmbed',
             {
                 label: 'Embed Media',
                 command: 'MediaEmbed',
-                icon: this.path + 'images/icon.png',
                 toolbar: 'mediaembed'
             } );
         }
