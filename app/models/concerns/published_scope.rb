1bb50ce082a47e5b2923be4a1db59a830b3cf60f
Bryan Ricker <bricker88@gmail.com>
2013-10-18 10:40:49 -0700

    Cleanup

--- app/models/concerns/published_scope.rb
+++ app/models/concerns/published_scope.rb
@@ -2,6 +2,8 @@ module PublishedScope
   extend ActiveSupport::Concern
 
   included do
-    scope :published, -> { where(status: self::STATUS[:published]).order("published_at desc") }
+    scope :published, -> {
+      where(status: self::STATUS[:published]).order("published_at desc")
+    }
   end
 end
