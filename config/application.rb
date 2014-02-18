acf7c181025c53f6376f119c1e1452612f5fba13
Bryan Ricker <bricker88@gmail.com>
2014-02-18 13:58:57 -0800

    Add :assets group to Bundler.require, remove from Gemfile

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
-Bundler.require(*Rails.groups(assets: %w(development test)))
+Bundler.require(:default, Rails.env, :assets)
 
 module AudioVision
   class Application < Rails::Application
