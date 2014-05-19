b8eef6fbd8b8e3a99cf1910388e622555b4432c2
Bryan Ricker <bricker88@gmail.com>
2014-05-19 08:16:41 -0700

    Fix error template prefix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include Outpost::Controller::CustomErrors
 
-  def render_error(status, e=StandardError)
+  def render_error(status, e=StandardError, template_prefix="")
     super
     report_error(e)
   end
