95f2febe246bdd863261692b25440cddf1d77d40
Bryan Ricker <bricker88@gmail.com>
2014-06-03 11:59:14 -0700

    Fix migration

--- db/migrate/20140603185139_remove_flatpages.rb
+++ db/migrate/20140603185139_remove_flatpages.rb
@@ -3,7 +3,7 @@ class RemoveFlatpages < ActiveRecord::Migration
     drop_table :flatpages
 
     p = Permission.find_by_resource("Flatpage")
-    UserPermission.where(permission_id: p.id).delete
+    UserPermission.where(permission_id: p.id).delete_all
     p.delete
   end
 end
