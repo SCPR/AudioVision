88c843c642104009fdf64c7d3295e3d1e0998100
Bryan Ricker <bricker88@gmail.com>
2014-08-17 13:58:37 -0700

    Remove report_error calls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include Outpost::Controller::CustomErrors
 
+  # This is here just to set the template_prefix option.
   def render_error(status, e=StandardError, template_prefix="")
     super
-    report_error(e)
   end
 end
--- app/controllers/outpost/base_controller.rb
+++ app/controllers/outpost/base_controller.rb
@@ -1,6 +1,2 @@
 class Outpost::BaseController < Outpost::ApplicationController
-  def render_error(status, e=StandardError)
-    super
-    report_error(e)
-  end
 end
